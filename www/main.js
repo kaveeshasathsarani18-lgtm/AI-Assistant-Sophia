766ba09bdbe45782eb4f3a9b84cdb1525ee2a90e
kavee726 <it23325500@my.sliit.lk>
2025-12-13 00:38:30 +0530

    is first part

---
+++ www/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.text').taxtillate({
+        loop: true,
+        sync:true,
+        in:{
+            effect: 'bounceIn',
+        },
+        out:{
+            effect: 'bounceOut',
+        },
+    });
+        
+});
